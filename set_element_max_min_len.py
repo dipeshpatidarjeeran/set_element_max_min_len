460bd2f8bc2632bf5f5d4f49cdf1abbeb0aa4556
Dipesh patidar <94466488+dipeshpatidarjeeran@users.noreply.github.com>
2022-04-26 18:34:36 +0530

    Add files via upload

---
+++ set_element_max_min_len.py
@@ -0,0 +1,6 @@
+s={5,3,6,7,91,2,4,5,677}
+print('set is:-',s)
+print("maximum no in set:-",max(s))
+print("minimum no in set:-",min(s))
+print("sum element in set:-",sum(s))
+print("length of set:-",len(s))
